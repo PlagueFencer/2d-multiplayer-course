5c16b4582d3c6e4ef0bd3d83ad00d2a691d96d93
markkukynsijarvi <markku.kynsijarvi@iki.fi>
2026-02-20 13:15:26 +0200

    bulletspawn speed and deletion

--- entities/bullet/bullet.gd
+++ entities/bullet/bullet.gd
@@ -2,7 +2,11 @@ class_name Bullet
 extends Node2D
 
 const SPEED: int = 600
+@onready var life_timer: Timer = $LifeTimer
 var direction: Vector2
+
+func _ready() -> void:
+	life_timer.timeout.connect(_on_life_timer_timeout)
 
 func _process(delta: float) -> void:
 	global_position += direction * SPEED * delta
@@ -10,3 +14,8 @@ func _process(delta: float) -> void:
 func start(_direction: Vector2):
 	self.direction = _direction
 	rotation = direction.angle()
+	
+func _on_life_timer_timeout():
+	# are we on server?
+	if is_multiplayer_authority():
+		queue_free()
